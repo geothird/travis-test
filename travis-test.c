a8f0aa94cadcd7d92a4a8496abe702ab3a49ff87
Geo <geo.marshall@gmail.com>
2015-08-19 16:14:51 -0600

    Commiting broken feature

--- travis-test.c
+++ travis-test.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
   if (argc != 5)
   {
     printf("\n\n---Test Travis: -----------------------\n");
+    broke the build;
   }
   printf("\n\n---End  Travis: -----------------------\n"); 
 }
