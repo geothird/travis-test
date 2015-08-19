4ba36d984baf464afc147862bdc8629b566aa3bc
Geo <geo.marshall@gmail.com>
2015-08-19 15:29:47 -0600

    Initial commit of test

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: travis-test.c
+	  gcc -g -Wall -o travis-test travis-test.c
---
+++ travis-test.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  if (argc != 5)
+  {
+    printf("\n\n---Test Travis: -----------------------\n");
+  }
+  printf("\n\n---End  Travis: -----------------------\n"); 
+}
